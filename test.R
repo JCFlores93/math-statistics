352f03c5cac63946218bbe0962d626da7d8ef844
Admin <Admin@LABCA408-PC21.labhard.urp.edu.pe>
2019-05-05 13:34:48 -0500

    first test

--- test.R
+++ test.R
@@ -1,2 +1,31 @@
 #getwd()
 setwd("D:/math-machinelearning")
+
+2+3
+5/9
+log(5)
+pi
+exp(2)
+sin(pi)
+escalar <- 10
+escalar
+escalar2 <- 10
+escalar + escalar2
+escalar ^ 2
+#numeric -> continuos
+class(10)
+class(1.5)
+
+#integer -> discretos
+class(5L)
+
+#factor -> nominal / ordinal 
+class('machine learning')
+
+obj1 <- 10 
+class(obj1)
+
+obj1 <- as.character(obj1)
+class(obj1)
+
+is.character(obj1)
