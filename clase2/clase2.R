b401b592fc86d900a437141c139274545074ba67
Admin <Admin@LABCA408-PC21.labhard.urp.edu.pe>
2019-05-19 10:07:55 -0500

    Uploading data

---
+++ clase2/clase2.R
@@ -0,0 +1,5 @@
+#Cargamos el conjunto de datos 
+telco <- read.csv('https://raw.githubusercontent.com/VictorGuevaraP/ME-Machine-Learning/master/Perdida%20de%20clientes.csv', sep = ';')
+
+#Observamos el conjunto de datos
+head(telco)
